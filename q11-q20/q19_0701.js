52f3ba64791f17c8f49d0b564f2a96aff2e0bd76
eunjae <>
2024-07-01 15:05:46 +0900

    js

---
+++ q11-q20/q19_0701.js
@@ -0,0 +1,6 @@
+//*flag에 따른 값 반환하기
+
+
+function solution(a, b, flag) {
+    return flag ? a+b : a-b
+}
--- q19_0701.js
+++
@@ -1,6 +0,0 @@
-//*flag에 따른 값 반환하기
-
-
-function solution(a, b, flag) {
-    return flag ? a+b : a-b
-}
