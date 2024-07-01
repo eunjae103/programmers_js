9295325b11ef9b7bdf2bf034f80b1b914d8471e3
eunjae <>
2024-07-01 15:46:44 +0900

    js

---
+++ q21_0701.js
@@ -0,0 +1,7 @@
+//*ad 제거하기
+
+
+function solution(strArr) {
+   
+    return strArr.filter(a => !a.includes("ad"))
+}
---
+++ q22_0701.js
@@ -0,0 +1,6 @@
+//*두 수의합
+
+function solution(a, b) {
+    const answer = String(BigInt(a) + BigInt(b));
+    return answer;
+}
