4c88ab767e89c230b086830a53e49ca900f4f40c
eunjae <>
2024-07-01 15:03:28 +0900

    js

---
+++ q17_0701.js
@@ -0,0 +1,13 @@
+//*홀짝에 따라 다른 값 반환하기
+
+function solution(n) {
+    var answer = 0;
+    if(n%2!=0){ // 홀수라면
+        for(var i = 1; i <= n; i+=2) 
+            answer += i;
+    }else{ // 짝수라면
+        for(var i = 2; i <= n; i+=2) 
+            answer += Math.pow(i,2);
+    }
+    return answer;
+}
---
+++ q18_0701.js
@@ -0,0 +1,8 @@
+//*조건문자열
+
+function solution(ineq, eq, n, m) {
+    if (eq === '=' && n === m) return 1
+    if (ineq === '<' && n < m) return 1
+    if (ineq === '>' && n > m) return 1
+    return 0
+}
